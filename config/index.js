aa65894db75ef397826be9d35ea743fc7f4e4412
fox <450499752@qq.com>
2018-02-26 20:08:54 +0800

    express

--- config/index.js
+++ config/index.js
@@ -10,10 +10,12 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api/':'http://localhost:9999'
+    },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '172.31.7.56', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
